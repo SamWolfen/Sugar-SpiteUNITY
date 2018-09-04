47672533fdb94f9f829242ebe24558b6f3862f28
SamWolfen <27791395+SamWolfen@users.noreply.github.com>
2018-09-04 12:30:46 +1000

    Initial commit

---
+++ Assets/SpiteHover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiteHover : MonoBehaviour {
+
+    public float Counter, sinCounter, Rate;
+    Vector3 Pos;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Pos = this.gameObject.transform.position;
+
+        Counter += Rate * Time.deltaTime;
+        sinCounter = Mathf.sin(Counter);
+
+        Pos += new Vector3(0, sinCounter, 0);
+
+
+        this.gameObject.transform.position = Pos;
+            }
+}
